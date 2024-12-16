43da9f8cc0451785ecf3ee360eb65952d2de8a3e
niyazahmad11787 <ahmadniyaz11787@gmail.com>
2024-12-16 16:33:44 +0530

    initial project setup

---
+++ src/constants/strings.ts
@@ -0,0 +1,38 @@
+class Strings {
+  // static telegram = "Telegram";
+  // static telegramLink = "https://telegram.me/nixrajput";
+  // static telegramUsername = "nixrajput";
+
+  static github = "GitHub";
+  static githubLink = "https://www.github.com/niyazahmad11787";
+  static githubUsername = "niyazahmad11787";
+
+  static linkedIn = "LinkedIn";
+  static linkedInLink = "www.linkedin.com/in/niyaz-ahmad-2b45b2249";
+  static linkedInUsername = "Niyaz Ahmad";
+
+  static instagram = "Instagram";
+  static instagramLink = "https://www.instagram.com/i_amniyaz_";
+  static instagramUsername = "i_amniyaz_";
+
+  // static twitter = "Twitter";
+  // static twitterLink = "https://www.twitter.com/nixrajput07";
+  // static twitterUsername = "nixrajput07";
+
+  static email = "Email";
+  static primaryEmailLink = "mailto:niyaz.ahmad11787@gmail.com";
+  static primaryEmail = "niyaz.ahmad11787@gmail.com";
+
+  // static buyMeACoffee = "BuyMeACoffee";
+  // static buyMeACoffeeLink = "https://buymeacoffee.com/nixrajput";
+  // static buyMeACoffeeUsername = "nixrajput";
+
+  // static koFi = "Ko-Fi";
+  // static koFiLink = "https://ko-fi.com/nixrajput";
+  // static koFiUsername = "nixrajput";
+
+  static fullName = "Niyaz Ahmad";
+  static shortName = "Niyaz";
+}
+
+export default Strings;
